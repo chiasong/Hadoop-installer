28f98b282d334450dfab1580a1a3e98eb5d5ca78
smallcoder <suhang1998@hotmail.com>
2021-12-27 21:44:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ install_ready() {
 	hostname node1
 	sed -i '$a\node1' /etc/hostname
 	sed -i '1d' /etc/hostname
-	
+	echo "正在修改hosts文件"
 	hosts_tmp=`cat /etc/hosts | grep node1`
 	if [[ ! $hosts_tmp =~ 'node1' ]];then
 	  ip_tmp=`ip addr`
@@ -54,7 +54,7 @@ install_ready() {
 	    ip=$(ip addr |grep inet |grep -v inet6 |grep ens33|awk '{print $2}' |awk -F "/" '{print $1}');
 	  elif [[ $ip_tmp =~ 'ens32' ]];then
 	    ip=$(ip addr |grep inet |grep -v inet6 |grep ens32|awk '{print $2}' |awk -F "/" '{print $1}');
-	fi
+	  fi
 	echo $ip;
 	sed -i '$a\'"$ip"' node1' /etc/hosts;
 	else
@@ -114,7 +114,7 @@ install_jdk() {
 	
 	
 	echo "开始解压jdk"
-	tar zxvf /export/software/jdk-8u241-linux-x64.tar.gz -C /export/server
+	tar zxf /export/software/jdk-8u241-linux-x64.tar.gz -C /export/server
 	echo "解压jdk完成"
 	echo "开始添加jdk环境变量"
 	sed -i '$a\JAVA_HOME=/export/server/jdk1.8.0_241\n\CLASSPATH=.:$JAVA_HOME/lib\n\PATH=$JAVA_HOME/bin:$PATH\n\export JAVA_HOME CLASSPATH PATH' /etc/profile
@@ -140,7 +140,7 @@ install_zookeeper() {
 	echo "zookeeper安装包已存在"
 	fi
 	
-	tar -zxf /export/software/zookeeper-3.4.6.tar.gz -C /export/server/
+	tar zxf /export/software/zookeeper-3.4.6.tar.gz -C /export/server/
 	mv /export/server/zookeeper-3.4.6 /export/server/zookeeper
 	mkdir -p /export/data/zkdata
 	cp /export/server/zookeeper/conf/zoo_sample.cfg /export/server/zookeeper/conf/zoo.cfg
@@ -254,7 +254,7 @@ install_hive() {
 	wget --no-check-certificate -P /export/software/ http://myfilehadoop.sgp1.cdn.digitaloceanspaces.com/apache-hive-3.1.2-bin.tar.gz
 	echo "下载完成"
 	echo "开始解压hive"
-	tar zxvf /export/software/apache-hive-3.1.2-bin.tar.gz -C /export/server/
+	tar zxf /export/software/apache-hive-3.1.2-bin.tar.gz -C /export/server/
 	mv /export/server/apache-hive-3.1.2-bin /export/server/hive-3.1.2
 	rm -rf /export/server/hive-3.1.2/lib/guava-19.0.jar
 	cp /export/server/hadoop-3.3.0/share/hadoop/common/lib/guava-27.0-jre.jar /export/server/hive-3.1.2/lib
@@ -286,7 +286,7 @@ install_spark() {
 	wget --no-check-certificate -P /export/software/ http://myfilehadoop.sgp1.cdn.digitaloceanspaces.com/spark-3.1.2-bin-hadoop3.2.tgz
 	echo "下载完成"
 	echo "开始解压spark"
-	tar zxvf /export/software/spark-3.1.2-bin-hadoop3.2.tgz -C /export/server/
+	tar zxf /export/software/spark-3.1.2-bin-hadoop3.2.tgz -C /export/server/
 	mv /export/server/spark-3.1.2-bin-hadoop3.2 /export/server/spark
 	echo "开始修改spark配置文件"
 	cp /export/server/spark/conf/spark-env.sh.template /export/server/spark/conf/spark-env.sh
@@ -318,6 +318,30 @@ install_spark() {
 	spark_install=false;
 	fi
 	/export/server/hadoop-3.3.0/sbin/stop-all.sh
+}
+
+install_sqoop() {
+	echo "开始安装sqoop"
+	echo "开始下载sqoop"
+
+	if [[ ! -f /export/software/sqoop-1.4.7.bin_hadoop-2.6.0.tar.gz ]]; then
+	wget --no-check-certificate -P /export/software http://myfilehadoop.sgp1.cdn.digitaloceanspaces.com/sqoop-1.4.7.bin_hadoop-2.6.0.tar.gz
+	echo "下载sqoop完成"
+	else
+	echo "sqoop安装包已存在"
+	fi
+	
+	echo "开始解压sqoop"
+	tar zxf /export/software/sqoop-1.4.7.bin_hadoop-2.6.0.tar.gz -C /export/server
+	mv /export/server/sqoop-1.4.7.bin__hadoop-2.6.0 /export/server/sqoop
+	echo "解压sqoop完成"
+	echo "开始添加sqoop环境变量"
+	sed -i '$a\export SQOOP_HOME=/export/server/sqoop' /etc/profile
+	source /etc/profile;
+	mv /export/server/sqoop/conf/sqoop-env-template.sh /export/server/sqoop/conf/sqoop-env.sh
+	sed -i '$a\export HADOOP_COMMON_HOME= /export/server/hadoop-3.3.0\n\export HADOOP_MAPRED_HOME= /export/server/hadoop-3.3.0\n\export HIVE_HOME= /export/server/hive-3.1.2' /export/server/sqoop/conf/sqoop-env.sh
+  cp /export/server/hive-3.1.2/lib/mysql-connector-java-5.1.32.jar /export/server/sqoop/lib/
+  wget -P /export/server/sqoop/lib http://suhang.work/hadoop_etc/commons-lang-2.6.jar
 }
 
 install_finsh() {
@@ -367,11 +391,12 @@ install_finsh() {
 	clear
   echo "========================"
 	if [[ java_install && zoo_install && mysql_install && hadoop_install && hive_install && spark_install ]]; then
-	echo "jdk+mysql_5.7+hadoop_3.3.0+zookeeper_3.4.6+hive_3.1.2+spark_3.1.2 已安装成功"
+	echo "jdk+mysql_5.7+hadoop_3.3.0+zookeeper_3.4.6+hive_3.1.2+spark_3.1.2+sqoop 已安装成功"
 	echo "mysql 的默认密码为:hadoop666"
 
 	fi
 }
+
 
 install_ready
 install_jdk
@@ -380,4 +405,5 @@ install_zookeeper
 install_hadoop
 install_hive
 install_spark
+install_sqoop
 install_finsh
